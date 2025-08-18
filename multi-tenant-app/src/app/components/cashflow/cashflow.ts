e18d6610e1f2b9d34c3fa2eecd94c07211497f50
Matt Wilhelm <mattswilhelm@hotmail.com>
2025-08-18 08:24:14 -0700

    Added base cash flow page model and layout with static data

--- multi-tenant-app/src/app/components/cashflow/cashflow.ts
+++ multi-tenant-app/src/app/components/cashflow/cashflow.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MoneyTransaction } from './models/money-transaction';
 
 @Component({
   selector: 'app-cashflow',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './cashflow.scss'
 })
 export class Cashflow {
+  temporaryStaticPageData: Array<MoneyTransaction> = [
+    this.generateMoneyTransactionRecord(),
+    this.generateMoneyTransactionRecord(),
+    this.generateMoneyTransactionRecord(),
+    this.generateMoneyTransactionRecord()
+  ];
+
+  generateMoneyTransactionRecord() : MoneyTransaction {
+    return Object.assign(new MoneyTransaction(), {
+      amount: 1234,
+      details: "Some stubbed details",
+      date: "11/11/1111",
+      accountNumber: "Ac123",
+      referenceNumber: "ref1013"
+    });
+  }
 
 }
