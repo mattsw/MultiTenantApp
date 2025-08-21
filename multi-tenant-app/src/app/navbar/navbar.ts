875c9e6da604cdadc18217d49e19e8d93204b836
Matt Wilhelm <mattswilhelm@hotmail.com>
2025-08-21 13:57:09 -0700

    Initial OOTB login button for auth0 added to navbar

--- multi-tenant-app/src/app/navbar/navbar.ts
+++ multi-tenant-app/src/app/navbar/navbar.ts
@@ -3,6 +3,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-navbar',
@@ -11,5 +12,8 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.scss'
 })
 export class Navbar {
+  
+  constructor(public authService: AuthService){
 
+  }
 }
