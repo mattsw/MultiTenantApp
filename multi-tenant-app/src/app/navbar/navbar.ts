05bfb5f3f5afe4d8237ac3d865baffa18270add4
Matt Wilhelm <mattswilhelm@hotmail.com>
2025-08-22 13:18:56 -0700

    Added logout to the application if a user is logged in

--- multi-tenant-app/src/app/navbar/navbar.ts
+++ multi-tenant-app/src/app/navbar/navbar.ts
@@ -4,16 +4,28 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatIconModule, MatButtonModule, MatToolbarModule, RouterLink],
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule, RouterLink, AsyncPipe],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
-  
+  public isLoggedIn: boolean = false;
   constructor(public authService: AuthService){
+    authService.isAuthenticated$.subscribe((data:boolean) =>{
+      this.isLoggedIn = data;
+    });
+  }
 
+  logout(){
+    console.log(this.authService.isAuthenticated$)
+    this.authService.logout({ 
+      logoutParams: {
+        returnTo: document.location.origin 
+      }
+    });
   }
 }
