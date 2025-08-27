70d8393463d1affa626142414c41d77c29e58ee9
Matt Wilhelm <mattswilhelm@hotmail.com>
2025-08-26 19:17:43 -0700

    Introduced a service layer for cashflow transactions and test proj

---
+++ MultiTenantApp.Web/MultiTenantApp.Test.Services/MoneyTransactionServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using MultiTenantApp.Services.Transactions;
+
+namespace MultiTenantApp.Test.Services
+{
+    [TestFixture]
+    public class MoneyTransactionServiceTests
+    {
+        [Test]
+        public void GetAllTransactions_ShouldReturnNonEmptyList()
+        {
+            // Arrange
+            var sut = new MoneyTransactionService();
+
+            // Act
+            var result = sut.GetAllTransactions();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().NotBeEmpty();
+        }
+    }
+}
--- MultiTenantApp.Web/MultiTenantApp.Web/Controllers/CashflowController.cs
+++ MultiTenantApp.Web/MultiTenantApp.Web/Controllers/CashflowController.cs
@@ -1,34 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MultiTenantApp.Domain.Cashflow;
+using MultiTenantApp.Services.Transactions;
 
 namespace MultiTenantApp.Web.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CashflowController : ControllerBase
+    public class CashflowController(IMoneyTransactionService moneyTransactionService) : ControllerBase
     {
         [HttpGet]
         public IActionResult Get()
-        {
-            // Placeholder for actual cashflow data retrieval logic
-            var transactions = new List<MoneyTransaction>
-            {
-                new() {
-                    Amount = 100.00m,
-                    Details = "Sample Transaction 1",
-                    Date = DateTime.UtcNow,
-                    AccountNumber = "123456789",
-                    ReferenceNumber = "REF001"
-                },
-                new() {
-                    Amount = 250.50m,
-                    Details = "Sample Transaction 2",
-                    Date = DateTime.UtcNow.AddDays(-1),
-                    AccountNumber = "987654321",
-                    ReferenceNumber = "REF002"
-                }
-            };
-            return Ok(transactions);
+        {            
+            return Ok(moneyTransactionService.GetAllTransactions());
         }
     }
 }
