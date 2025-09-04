1d0049e68c00e9fb77a7d6f476326f7d3b12166f
Matt Wilhelm <mattswilhelm@hotmail.com>
2025-09-03 20:50:42 -0700

    Fixed broken unit tests and exe output of repository proj

---
+++ MultiTenantApp.Test.Repositories/MoneyTransactionRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using MultiTenantApp.Repositories.Cashflow;
+
+namespace MultiTenantApp.Test.Repositories;
+
+public class MoneyTransactionRepositoryTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void MoneyTransactionRepository_ShouldInstantiate()
+    {
+        var sut = new MoneyTransactionRepository();
+        Assert.That(sut, Is.Not.Null);
+    }
+}
