870c56fa6718adc0cf88e5432440416000380d87
Matt Wilhelm <mattswilhelm@hotmail.com>
2025-09-03 19:54:27 -0700

    Added base skeleton for data access layer

---
+++ MultiTenantApp.Repositories/Cashflow/MoneyTransactionRepository.cs
@@ -0,0 +1,17 @@
+﻿using MultiTenantApp.Domain.Cashflow;
+
+namespace MultiTenantApp.Repositories.Cashflow;
+
+public interface IMoneyTransactionRepository
+{
+    IEnumerable<MoneyTransaction> GetAllTransactions();
+}
+
+public sealed class MoneyTransactionRepository : IMoneyTransactionRepository
+{
+    public IEnumerable<MoneyTransaction> GetAllTransactions()
+    {
+        throw new NotImplementedException();
+    }
+}
+
