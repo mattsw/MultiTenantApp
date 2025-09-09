9f87baa07118175c90338042226abc54a2750911
Matt Wilhelm <mattswilhelm@hotmail.com>
2025-09-09 00:06:38 -0700

    Added dataflow controller with hard coded transaction response

---
+++ MultiTenantApp.Web/MultiTenantApp.Domain/Cashflow/DataflowTransaction.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiTenantApp.Domain.Cashflow;
+
+public sealed class DataflowTransaction
+{
+    public int TransactionId { get; set; }
+    public string Source { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public string Details { get; set; } = string.Empty;
+}
---
+++ MultiTenantApp.Web/MultiTenantApp.Web/Controllers/DataflowController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiTenantApp.Domain.Cashflow;
+
+namespace MultiTenantApp.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DataflowController : ControllerBase
+{
+    [HttpGet("status")]
+    public IActionResult GetStatus()
+    {
+        var transactions = new List<DataflowTransaction>
+        {
+            new() 
+            {
+                TransactionId = 1,
+                Source = "Dataflow A",
+                Date = DateTime.UtcNow.AddDays(-1),
+                Details = "Transaction details for Dataflow A"
+            },
+            new() 
+            {
+                TransactionId = 2,
+                Source = "Dataflow B",
+                Date = DateTime.UtcNow.AddDays(-2),
+                Details = "Transaction details for Dataflow B"
+            }
+        };
+        return Ok(transactions);
+    }
+}
